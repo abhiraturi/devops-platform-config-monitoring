9ee1c858847afa8b5de620b11e03f8d6b79cc254
abhiraturi <abhishek.raturi6@gmail.com>
2026-06-12 11:22:52 -0400

    added decoded cert in providers

--- providers.tf
+++ providers.tf
@@ -28,6 +28,7 @@ provider "helm" {
     host                   = data.azurerm_kubernetes_cluster.aks.kube_config.0.host
     client_certificate     = base64decode(data.azurerm_kubernetes_cluster.aks.kube_config.0.client_certificate)
     client_key             = base64decode(data.azurerm_kubernetes_cluster.aks.kube_config.0.client_key)
-    cluster_ca_certificate = data.azurerm_kubernetes_cluster.aks.kube_config.0.cluster_ca_certificate
+    # THIS LINE WAS LIKELY MISSING THE DECODE FUNCTION:
+    cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.aks.kube_config.0.cluster_ca_certificate)
   }
 }
