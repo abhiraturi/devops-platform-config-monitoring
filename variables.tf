464cc5b1e56c97fc148b1df81090b9da0bbcdd41
abhiraturi <abhishek.raturi6@gmail.com>
2026-06-12 11:05:04 -0400

    updated deploy yaml

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "aks_name" {
+  description = "The name of the AKS cluster"
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "The name of the resource group containing the AKS cluster"
+  type        = string
+}
